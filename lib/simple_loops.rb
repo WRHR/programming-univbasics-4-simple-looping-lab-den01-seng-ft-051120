097330da49b477809c663d711a9e3069f16f56c2
Will Reeves <wrhreeves@gmail.com>
2020-04-08 19:25:05 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,26 @@
 # Write your methods here
+def loop_message_five_times(string)
+  5.times {puts(string)}
+end
+
+def loop_message_n_times(string, num)
+  num.times {puts(string)}
+end
+
+def output_array(arr)
+  index = 0 
+  while index < arr.length do
+    puts arr[index]
+    index += 1
+  end
+end
+
+def return_string_array(arr)
+  string_arr = []
+  i = 0 
+  while i < arr.length do 
+    string_arr << arr[i].to_s 
+    i += 1
+  end
+  string_arr
+end
